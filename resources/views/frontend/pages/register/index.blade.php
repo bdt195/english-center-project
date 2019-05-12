f23602a20e7ca2e83c927dca902e673f4ba05015
vietnt94v <vietnt94v@gmail.com>
2019-05-12 20:44:42 +0700

    edit form register

--- resources/views/frontend/pages/register/index.blade.php
+++ resources/views/frontend/pages/register/index.blade.php
@@ -4,34 +4,46 @@
   <div class="container">
     <div class="pt-5"></div>
     <div class="register-form">
-      <h1 class="title">Register</h1>
+      <h1 class="title">Đăng kí tài khoản</h1>
       <form action="#">
         <form>
           <div class="form-group">
-            <label for="registerEmail">Email address</label>
-            <input class="form-control" id="registerEmail" type="email" placeholder="Enter email">
+            <label for="registerEmail">Địa chỉ Email</label>
+            <input class="form-control" id="registerEmail" type="email" placeholder="Nhập email">
           </div>
           <div class="form-group">
-            <label for="registerPassword">Password</label>
-            <input class="form-control" id="registerPassword" type="password" placeholder="Password">
+            <label for="registerPassword">Mật khẩu</label>
+            <input class="form-control" id="registerPassword" type="password" placeholder="Nhập mật khẩu">
           </div>
           <div class="form-group">
-            <label for="retypeRegisterPassword">Retype Password</label>
-            <input class="form-control" id="retypeRegisterPassword" type="password" placeholder="Retype Password">
+            <label for="retypeRegisterPassword">Nhập lại mật khẩu</label>
+            <input class="form-control" id="retypeRegisterPassword" type="password" placeholder="Nhập lại mật khẩu">
           </div>
           <div class="form-group">
-            <label for="registerName">Full Name</label>
-            <input class="form-control" id="registerName" type="text" placeholder="Full Name">
+            <label for="registerLastName">Họ</label>
+            <input class="form-control" id="registerLastName" type="text" placeholder="Họ">
           </div>
           <div class="form-group">
-            <label for="registerName">Phone Number</label>
-            <input class="form-control" id="registerPhone" type="text" placeholder="PhoneNumber">
+            <label for="registerName">Tên</label>
+            <input class="form-control" id="registerName" type="text" placeholder="Tên">
+          </div>
+          <div class="form-group">
+            <label for="registerBirthday">Ngày sinh</label>
+            <input class="form-control" id="registerBirthday" type="date">
+          </div>
+          <div class="form-group">
+            <label for="registerName">Số điện thoại</label>
+            <input class="form-control" id="registerPhone" type="text" placeholder="Số điện thoại">
+          </div>
+          <div class="form-group">
+            <label for="registerAddress">Địa chỉ nơi ở</label>
+            <input class="form-control" id="registerAddress" type="email" placeholder="Địa chỉ của bạn">
           </div>
           <div class="form-group form-check">
             <input class="form-check-input" id="agreeRules" type="checkbox">
-            <label class="form-check-label" for="agreeRules">Agree with our rules!</label>
+            <label class="form-check-label" for="agreeRules">Tôi hoàn toàn đồng ý với những điều khoản của trung tâm!</label>
           </div>
-          <button class="btn btn-primary" id="btn-register" type="submit">Register</button>
+          <button class="btn btn-primary" id="btn-register" type="submit">Đăng kí</button>
         </form>
       </form>
     </div>
