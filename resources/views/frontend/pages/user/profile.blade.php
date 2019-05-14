c3856604ff30c5f0bbc19b8e916b0a5a7b49947e
vietnt94v <vietnt94v@gmail.com>
2019-05-15 06:21:09 +0700

    add anh avatar trong  user profile

--- resources/views/frontend/pages/user/profile.blade.php
+++ resources/views/frontend/pages/user/profile.blade.php
@@ -5,6 +5,7 @@
     <div class="py-5">
       <div class="row">
         <div class="col-3">
+          <div class="avatar-profile" style="background-image: url('{{asset('frontend/img/course/course-1.jpg')}}')"></div>
           <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
             <a class="nav-link active" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="true">Thông tin cá nhân</a>
             <a class="nav-link" id="v-pills-edit-profile-tab" data-toggle="pill" href="#v-pills-edit-profile" role="tab" aria-controls="v-pills-edit-profile" aria-selected="true">Sửa thông tin cá nhân</a>
